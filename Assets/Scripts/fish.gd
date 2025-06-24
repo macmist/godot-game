63675c5bd6d12ce6a7547eb7c87b9a8bb8faa911
Quentin Musy <quentin.musy@gmail.com>
2025-06-24 19:07:09 +0100

    feat: fish has a health bar

--- Assets/Scripts/fish.gd
+++ Assets/Scripts/fish.gd
@@ -3,6 +3,12 @@ extends CharacterBody2D
 @export var speed: float = -100;
 @export var health: float = 100;
 
+@onready var health_bar: ProgressBar = %HealthBar
+
+func _ready() -> void:
+	health_bar.max_value = health
+	health_bar.value = health
+
 func _physics_process(_delta: float) -> void:
 	velocity = Vector2(speed, 0)
 	move_and_slide()
@@ -10,5 +16,6 @@ func _physics_process(_delta: float) -> void:
 
 func take_damage(damage: float):
 	health -= damage;
+	health_bar.value = health
 	if health <= 0:
 		queue_free()
