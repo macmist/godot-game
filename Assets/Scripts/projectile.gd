cc0b8344cf34db84dcc0c4f3b1bc6534967b459c
Quentin Musy <quentin.musy@gmail.com>
2025-07-01 23:32:26 +0100

    chore: make projectile disappear if target is dead

--- Assets/Scripts/projectile.gd
+++ Assets/Scripts/projectile.gd
@@ -6,11 +6,17 @@ extends Sprite2D
 @export var damage: float = 25
 @export var target: Fish
 
+
 func _process(delta: float) -> void:
+	# todo: this makes the projectiles homing. Would be nice to have some that goes in straight lines
+	# or some other fun behaviours
 	if target and is_instance_valid(target):
 		var direction = (target.global_position - global_position).normalized()
 		look_at(target.position)
 		position += direction * speed * delta
+	else:
+		# todo: make projectile continue and disappear after x time or when it touches something else
+		queue_free()
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
@@ -18,4 +24,3 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 		if body.has_method("take_damage"):
 			body.take_damage(damage)
 			queue_free()
-	pass # Replace with function body.
--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # Godot game
 
 Trying some stuff with godot.
-For now it's only following the tutorial from https://www.youtube.com/watch?v=W8gYHTjDCic
-More to come soon though
+Looks like a sort of tower defense but with other bits
 
 
 ## Arts
